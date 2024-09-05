7402c6e389ab9b99fa24482b905584ee443dd175
stefano <sgaruti@gmail.com>
2024-09-05 12:31:09 +0200

    Initial commit

---
+++ wordattach-for-cf7.php
@@ -0,0 +1,9 @@
+/*
+ * Plugin Name:       WordAttach for cf7
+ * Plugin URI:        https://bitbucket.org/ottomedia/wordattach-for-cf7/
+ * Requires at least: 6.5
+ * Requires PHP:      8.0
+ * Author:            Stefano Garuti
+ * Requires Plugins:  contact-form-7
+ * Version:           0.1.0
+ */
