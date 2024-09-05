1226c13bdd4be0ffd5b67b91e685b1f27cb1d5c4
stefano <sgaruti@gmail.com>
2024-09-05 17:28:55 +0200

    prima prototipo funzionante

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Work in progress #
 
+1. Crea il file "modello" in Word e salvalo col nome `template-form-_N_` dove _N_ è l'ID del form CF7 che raccoglie i dati per compilarlo
+1. Metti il file di word che fa da template nella cartella `wp-content/uploads/wpcf7-templates`. 
+
+
+I file "compilati" vengono salvati temporaneamente in `wp-content/uploads/wpcf7-templates/merged` e inviati via email.
+
+Al momento vengono allegati solo alla _Mail 1_ ... vediamo se abilitare l'invio ad entrambe con uno switch
+
+## todo ##
+1. cancellare i file "compilati" dalla cartella temporaneo dopo l'invio (magari mettere uno switch per decidere se farlo o no)
+2. creare le cartelle con il file .htaccess all'attivazione del plugin
+
 [PHPWord](https://phpoffice.github.io/PHPWord/index.html)
+
 [Stack Excange reference](https://stackoverflow.com/questions/48189010/dynamically-attaching-file-to-contact-form-7-e-mail)
--- wordattach-for-cf7.php
+++ wordattach-for-cf7.php
@@ -6,5 +6,113 @@
  * Requires PHP:      8.0
  * Author:            Stefano Garuti
  * Requires Plugins:  contact-form-7
- * Version:           0.1.0
+ * Version:           1.0.0
  */
+
+add_action( 'plugins_loaded', 'autoload_library', 0 );
+function autoload_library(){
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+
+add_action('wpcf7_before_send_mail', 'cv_word_doc');
+function cv_word_doc($WPCF7_ContactForm) {
+
+	$template_dir = wp_upload_dir();
+	$template_dir = $template_dir['basedir'] . '/wpcf7-templates/';
+
+	$template_name = 'template-form-' . $WPCF7_ContactForm->id() . '.docx';
+
+	if( file_exists( $template_dir . $template_name ) ) {
+
+		//Get current form
+		$wpcf7 = WPCF7_ContactForm::get_current();
+
+		// get current SUBMISSION instance
+		$submission = WPCF7_Submission::get_instance();
+
+		if ($submission) {
+
+			// get submission data
+			$data = $submission->get_posted_data();
+
+			// nothing's here... do nothing...
+			if (empty($data))
+				return;
+
+			// PHPWord stuff...
+			$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor( $template_dir . $template_name );
+
+			$templateProcessor->setValues($data);
+
+			// setup upload directory and name the file
+			$upload_dir = $template_dir . 'merged/';
+			$fileName = 'documento-' . time() .'.docx';
+
+			$templateProcessor->saveAs( $upload_dir . $fileName );
+
+			// add upload to e-mail
+
+			$submission->add_extra_attachments( $upload_dir . $fileName );
+			// $submission->add_extra_attachments( $upload_dir . $fileName, 'mail_2' );  //per inviare anche a email2
+
+			// carry on with cf7
+			return $wpcf7;
+
+		}
+	}
+}
+
+/*
+add_action(
+	'shutdown',
+	'wpcf7_cleanup_upload_files',
+	20, 0
+);
+*/
+/**
+ * Cleans up files in the temporary directory for uploaded files.
+ *
+ * @param int $seconds Files older than this are removed. Default 60.
+ * @param int $max Maximum number of files to be removed in a function call.
+ *                 Default 100.
+
+function wpcf7_cleanup_upload_files( $seconds = 60, $max = 100 ) {
+	$dir = trailingslashit( wpcf7_upload_tmp_dir() );
+
+	if ( ! is_dir( $dir )
+	or ! is_readable( $dir )
+	or ! wp_is_writable( $dir ) ) {
+		return;
+	}
+
+	$seconds = absint( $seconds );
+	$max = absint( $max );
+	$count = 0;
+
+	if ( $handle = opendir( $dir ) ) {
+		while ( false !== ( $file = readdir( $handle ) ) ) {
+			if ( '.' == $file
+			or '..' == $file
+			or '.htaccess' == $file ) {
+				continue;
+			}
+
+			$mtime = @filemtime( path_join( $dir, $file ) );
+
+			if ( $mtime and time() < $mtime + $seconds ) { // less than $seconds old
+				continue;
+			}
+
+			wpcf7_rmdir_p( path_join( $dir, $file ) );
+			$count += 1;
+
+			if ( $max <= $count ) {
+				break;
+			}
+		}
+
+		closedir( $handle );
+	}
+}
+*/
